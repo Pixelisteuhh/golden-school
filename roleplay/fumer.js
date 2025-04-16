5b74f9000f5b1a1bdd45e35f03cbdd22901dbac9
𝙰𝚍𝚊𝚖 <82734867+Pixelisteuhh@users.noreply.github.com>
2025-04-17 01:00:46 +0200

    Update fumer.js
    Signed-off-by: 𝙰𝚍𝚊𝚖' <82734867+Pixelisteuhh@users.noreply.github.com>

--- roleplay/fumer.js
+++ roleplay/fumer.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const mongoose = require('mongoose');
-const fumer = require("../fumer");
+const fumer = require("./fumer");
 const config = require('../config')
 const cl = require('quick.db').table("Color");
 const footer = config.bot.footer;
