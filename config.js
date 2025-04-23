2881ccacc8264e73e1a7934a58e196af02777fc0
𝙰𝚍𝚊𝚖 <82734867+Pixelisteuhh@users.noreply.github.com>
2025-04-23 22:41:17 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
         prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
-        couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
-        maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
+        buyer: ['721039033500827699', '1086596778788401182'], // Buyer
+        couleur: '#CD6F57', // Couleur HEX
+        footer: 'Golden School', // Dans la commande help ou autre il y'a un texte en bas
+        maxServer: '1', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
